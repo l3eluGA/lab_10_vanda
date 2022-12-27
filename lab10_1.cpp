1133a60c1e100495d25d13756d039e2c0beb20fd
l3eluGA <rungthiporange@gmail.com>
2022-12-27 16:32:29 +0700

    completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,13 +3,15 @@
 using namespace std;
 
 int main(){	
+	double loan,rate,amount,interest,total,nb=0;
+	int i=1;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
+	cin >> amount;
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
 	cout << setw(13) << left << "EndOfYear#"; 
 	cout << setw(13) << left << "PrevBalance"; 
 	cout << setw(13) << left << "Interest"; 
@@ -17,17 +19,34 @@ int main(){
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
+	cout << fixed << setprecision(2);
+	while(true)
+	{
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << loan;
+		interest=loan*(rate/100);
+		cout << setw(13) << left << interest;
+		total=loan+interest;
+		cout << setw(13) << left << total;
+		if(total<=amount) amount=total;
+		cout << setw(13) << left << amount;
+		nb= total-amount;
+		cout << setw(13) << left << nb;
+		loan=nb;
+		i++;
+		cout << "\n";
+		if(nb==0) break;
+		
+	}
 	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
+	/*cout << fixed << setprecision(2);  //fixทศนิยมสองตำแหน้ง
 	cout << setw(13) << left << 1; 
 	cout << setw(13) << left << 1000.0;
 	cout << setw(13) << left << 50.0;
 	cout << setw(13) << left << 1050.0;
 	cout << setw(13) << left << 100.0;
 	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	cout << "\n";*/
 	
 	return 0;
 }
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -1,8 +1,24 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
-
+void printO(int N,int M)
+{
+	if(N<=0 || M<=0)
+	{
+		cout << "Invalid input";
+	}
+	else
+	{
+		for(int i=0;i<N;i++)
+		{
+			for(int j=0;j<M;j++)
+			{
+				cout <<'O';
+			}
+			cout <<"\n";
+		}
+	}
+}
 int main(){
 	
 	printO(2,2);
